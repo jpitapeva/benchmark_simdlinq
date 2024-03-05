29b304649256f840fbd0c3281c890d832f664d0b
joaopma <joaopaulo.m@terra.com.br>
2024-03-05 17:22:24 -0300

    push

---
+++ Benchmark/Benchmark.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark
+{
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        private static readonly int[] Array = Enumerable.Range(1, 10000).ToArray();
+
+        [Benchmark]
+        public int ForLoopExample()
+        {
+            Span<int> span = Array;
+            var length = span.Length;
+            var result = 0;
+            for (int i = 0; i < length; i++)
+            {
+                result += span[i];
+            }
+            return result;
+
+        }
+
+        [Benchmark]
+        public int LinqExample()
+        {
+            return Array.Sum();
+        }
+
+        [Benchmark]
+        public int SimdLinqExample()
+        {
+            return SimdLinq.SimdLinqExtensions.Sum(Array);
+        }
+    }
+}
---
+++ Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿//var array = Enumerable.Range(1,2000).ToArray();
+
+//var sum = array.Sum();
+
+//Console.WriteLine(sum.ToString());
+
+
+using Benchmark;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+
+
